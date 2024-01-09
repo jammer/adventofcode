d7e41c21444a64ccda4df29bb69b69b64e74fb1f
Jammer <jammer@users.noreply.github.com>
2024-01-09 07:53:25 +0200

    9b

---
+++ 9b/src/main.rs
@@ -0,0 +1,46 @@
+fn calc(mut nums: Vec<i64>) -> Vec<i64> {
+    let first = nums[0].to_owned();
+    nums.reverse();
+    let mut second: i64 = nums.pop().unwrap();
+    let mut newnums: Vec<i64> = vec![];
+    let mut allzeroes = true;
+    while !nums.is_empty() {
+        let first = second;
+        second = nums.pop().unwrap();
+        newnums.push(second - first);
+        if second - first != 0 {
+            allzeroes = false;
+        }
+    }
+    if !allzeroes {
+        let mut vec = calc(newnums);
+        vec.push(first - vec.last().unwrap());
+        return vec;
+    }
+    vec![first]
+}
+
+fn calculate() -> i64 {
+    let contents = std::fs::read_to_string("input.txt").expect("unable to read input.txt");
+    let mut sum: i64 = 0;
+    for line in contents.lines() {
+        let nums: Vec<i64> = line
+            .split(" ")
+            .filter(|x| x.len() > 0)
+            .map(|x| x.parse().unwrap())
+            .collect();
+        let vec = calc(nums);
+        sum += vec.last().unwrap();
+    }
+    return sum;
+}
+
+fn main() {
+    let result = calculate();
+    println!("{result}");
+}
+
+#[test]
+fn correct_result() {
+    assert_eq!(calculate(), 1112);
+}
